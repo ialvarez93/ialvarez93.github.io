993d290869002395346b5a6f09b8e1574825e438
Ivan Alvarez <ivalvarez22@gmail.com>
2023-11-21 23:35:44 +0000

    Revert "build(nuxt): add repo name"
    This reverts commit c2f48b35cbca7e0621a28c655835ffbf3b96877f.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,6 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: "static",
-  router: {
-    base: '/ialvarez93.github.io/'
-  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
