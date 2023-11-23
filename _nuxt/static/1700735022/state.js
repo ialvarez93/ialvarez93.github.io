ab7be3683bb23d660e0053f6f2413dfadea41a65
Ivan Alvarez <ivalvarez22@gmail.com>
2023-11-23 10:24:48 +0000

    4a9616f

---
+++ _nuxt/static/1700735022/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700735022",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"10891f8e"}},_img:{"/_ipx/_/author.png":"\u002F_nuxt\u002Fimage\u002Ff10964.png"}}}(null,"\u002F"));
