dc9bc189ab3237b0466848527dc5887eea5e7b50
Ivan Alvarez <ivalvarez22@gmail.com>
2024-01-16 14:43:10 +0000

    feat(timeline): data update

--- components/TimeLine.vue
+++ components/TimeLine.vue
@@ -14,7 +14,7 @@
         <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Started on December
           2023</time>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Landing pages and simple websites.
+          Landing pages and simple websites with Nuxt.
         </p>
       </li>
       <li class="mb-10 ml-6">
@@ -26,30 +26,38 @@
           Programmer at biomercados (Alimentos FM)
         </h3>
         <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2022 - 2023</time>
+
+        <!-- <ul class="list-none hover:list-disc"> -->
         <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          <!-- <ul class="list-none hover:list-disc"> -->
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          Assessment, recovery and completion of an E-commerce project that involved deploying an Ionic Angular PWA,
-          an Android and iOS app for more than 30.000 users.
+          Assessment, recovery and completion of an E-commerce project that
+          involved deploying an Ionic Angular PWA, an Android and iOS app for
+          more than 30.000 users.
         </p>
         <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-          Designed, planed, implemented and managed docker build and image registry with a simple continuous delivery
-          pipeline for node.js express APIs and Angular PWA with ngnix
-        </P>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> Designed, Planned and Implemented headless
-          Magento 2 with docker containers
-          RFM analysis for ecommerce consumers for customer segmentation</P>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> Automated exchange rate extraction as web
-          scrapping from Venezuelan Central Bank Website
-          (https://www.bcv.org.ve/)</P>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> Designed and implemented CLI for
-          generating Email Signatures given an SVG and csv file</P>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> Wrote clear and consistent technical
-          documentation</P>
-        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"> Gave consumer and costumer technical
-          support</P>
+          Designed, planed, implemented and managed docker build and image
+          registry with a simple continuous delivery pipeline for node.js
+          express APIs and Angular PWA with ngnix
+        </p>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Designed, Planned and Implemented headless Magento 2 with docker
+          containers RFM analysis for E-commerce consumers for customer
+          segmentation
+        </p>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Automated exchange rate extraction as web scrapping from Venezuelan
+          Central Bank Website (https://www.bcv.org.ve/)
+        </p>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Designed and implemented CLI for generating Email Signatures given an
+          SVG and CSV file
+        </p>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Wrote clear and consistent technical documentation
+        </p>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Gave consumer and costumer technical support
+        </p>
         <!-- </ul> -->
-        </p>
       </li>
       <li class="mb-10 ml-6">
         <span
@@ -59,10 +67,14 @@
         <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">
           Freelancer
         </h3>
-        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Started on December
-          7th, 2021</time>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Started on December,
+          2021</time>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          Assessment, and completion of a multi tenant E-commerce website on CS-cart involving
+          theme development and custom plugin modifications for performance increase.
+        </p>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Landing pages and simple websites.
+          Landing pages and simple websites with WordPress.
         </p>
       </li>
       <li class="mb-10 ml-6">
@@ -76,7 +88,8 @@
         <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">2016 - 2021
         </time>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          Studied "Information Engineering" in Romulo Gallego's University (UNERG).
+          Studied "Information Engineering" in Romulo Gallego's University
+          (UNERG).
         </p>
       </li>
       <li class="mb-10 ml-6">
@@ -90,7 +103,7 @@
         <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Valencia,
           Venezuela</time>
         <p class="text-base font-normal text-gray-500 dark:text-gray-400">
-          I can't really remember any productive work in that time. <br>
+          I can't really remember any productive work in that time. <br />
           I'm fairly certain that my mother was the one doing the labor.
         </p>
       </li>
