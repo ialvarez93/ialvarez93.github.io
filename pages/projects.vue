ee0572992ddfc117862a54a7641f5455df236a98
Ivan Alvarez <ivalvarez22@gmail.com>
2024-01-16 14:27:50 +0000

    feat(projects): change title size

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,30 +1,30 @@
 <template>
   <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
     <div class="pt-6 pb-8 space-y-2 md:space-y-5">
-      <h1
+      <!-- <h1
         class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14"
       >
         Projects
       </h1>
       <p class="text-lg leading-7 text-gray-500 dark:text-gray-400">
         Some of the projects I've build or been a part of.
-      </p>
+      </p> -->
+      <header class="pt-16 pb-9 sm:pb-16 sm:text-center">
+        <h1 class="mb-4 text-8xl sm:text-4xl tracking-tight text-slate-800 font-extrabold dark:text-slate-200">
+          Projects
+        </h1>
+        <p class="text-lg text-slate-700 dark:text-slate-400">
+          Some of the projects I've build or been a part of.
+        </p>
+      </header>
     </div>
     <main class="relative mb-auto">
       <div class="">
         <div class="container py-12">
           <div class="flex flex-wrap -m-4">
-            <project-card
-              v-for="item in projectsData"
-              :key="item.title"
-              :title="item.title"
-              :description="item.description"
-              :href="item.href"
-              :github="item.github"
-              :tech1="item.tech1"
-              :tech2="item.tech2"
-              :tech3="item.tech3"
-            ></project-card>
+            <project-card v-for="item in projectsData" :key="item.title" :title="item.title"
+              :description="item.description" :href="item.href" :github="item.github" :tech1="item.tech1"
+              :tech2="item.tech2" :tech3="item.tech3"></project-card>
           </div>
         </div>
       </div>
