1ae022a3dc1e4a9cde329a39f7712bae41e1d13c
Ivan Alvarez <ivalvarez22@gmail.com>
2024-01-19 01:59:32 +0000

    update AuthorIntro.vue

--- components/AuthorIntro.vue
+++ components/AuthorIntro.vue
@@ -4,17 +4,18 @@
       👋 Hola, Good Day!
     </h2>
     <p class="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
-      I am a junior software developer with two years of experience in developing and maintaining ecommerce websites using
-      the PHP ecosystem.
+      As a junior full stack software engineer, I enjoy developing scalable web applications that can adapt to changing
+      customer needs and market trends. I have hands-on experience in building composable commerce solutions, using
+      various technologies in the PHP ecosystem to create customized and flexible E-commerce platforms.
       <br>
       <br>
-      I am a team player, a fast learner, and a problem solver. I enjoy working on challenging and meaningful projects
-      that can make a positive impact on the world. I am also a curious and avid reader who likes to keep up with the
-      latest trends and developments in data engineering and related fields.
+      I'm always eager to learn new technologies and frameworks, and I enjoy working in a collaborative and agile
+      environment. I'm looking for opportunities to apply my skills and experience to create impactful and user-friendly
+      solutions.
       <br>
       <br>
-      If you are looking for a junior data engineer who is enthusiastic, motivated, and ready to learn, please feel free
-      to contact me. I would love to hear from you and discuss how I can add value to your organization. Thank you for
-      your time and consideration.
+      I am also a curious and avid reader, movie goer and Japanophile. I like to explore different cultures and
+      perspectives, and I'm always open to new challenges.
     </p>
-  </div></template>
+  </div>
+</template>
